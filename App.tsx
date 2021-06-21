4cb7178304dd5d974f3762bbc72c32ddc2f98239
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-06-20 21:00:10 -0300

    Config Fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
 //React
 import "react-native-gesture-handler";
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+//Expo
+import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
+
+//Fonts
+import { useFonts } from "expo-font";
+import { Inter_400Regular } from "@expo-google-fonts/inter";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
