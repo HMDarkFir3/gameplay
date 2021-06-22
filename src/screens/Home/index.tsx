e86e0af72df5b52aef43d35873d4659434fadd4a
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-06-22 17:23:52 -0300

    Home Screen Done

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -34,6 +34,45 @@ export default function Home() {
       description:
         "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
     },
+    {
+      id: "2",
+      guild: {
+        id: "2",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "2",
+      date: "22/06 às 20:40h",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+    {
+      id: "3",
+      guild: {
+        id: "3",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "3",
+      date: "22/06 às 20:40h",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+    {
+      id: "4",
+      guild: {
+        id: "4",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "4",
+      date: "22/06 às 20:40h",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
   ];
 
   function handleCategorySelect(categoryId: string) {
